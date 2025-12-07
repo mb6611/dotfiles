631a4eb18e151af9fc23bab5c2e87b73501d35fe
Matthew Banaag <mb6611@princeton.edu>
2025-12-06 19:47:40 -0500

    Add .config/nvim/lua/plugins/neoscroll.lua

---
+++ dot_config/nvim/lua/plugins/neoscroll.lua
@@ -0,0 +1,7 @@
+return {
+  "karb94/neoscroll.nvim",
+  opts = {
+    duration_multiplier = 3.75,  -- Higher = slower scrolling (default is 1.0)
+    easing = "sine",       -- Smoother easing curve
+  },
+}
